8e211f52083eb411de00eb4dcea335576e81406c
jayaprasathjp <jayaprasathjp44@gmail.com>
2024-04-22 15:37:50 +0530

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import {Inter, Lusitana} from'next/font/google';
+export const inter=Inter({subsets:['latin']});
+export const lusitana=Lusitana({
+    weight:['400','700'],
+    subsets:['latin']
+});
